cbae47162cde7a9dbe882472ae1c5efebcc61ebb
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-11-05 20:55:44 -0500

    work: Voy a enfocarme en crear el registro de alumnos, luego el login

--- login/views.py
+++ login/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm 
 
 # Create your views here.
 def login_page(request):
-    return render(request, 'login.html')
+    return render(request, 'login.html', {
+        "form": UserCreationForm
+    })
