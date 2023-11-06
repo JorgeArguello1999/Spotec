93cfb69d2714dba3ba89ec161f1b8e5c92a2b51a
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-11-06 00:12:06 -0500

    feat: Crear y listar funcionando, trabajando en actualizar y eliminar

--- create_event/views.py
+++ create_event/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import create_evento_form
 from .models import create_evento
 
@@ -6,10 +6,16 @@ from .models import create_evento
 def list(request):
     lista = create_evento.objects.all()
     return render(request, 'list.html', {
-        "list": lista
+        "lista": lista
     })
 
 def create(request):
+    if request.method != "GET":
+        salida = create_evento_form(request.POST)
+        salida.save()
+        return redirect('list')
+
     return render(request, 'create.html', {
         "form": create_evento_form
     })
+
