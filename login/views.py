ba13b3d42013b0d1e20d00625dcf8e2647690931
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-11-05 22:20:29 -0500

    work: quiero darme un tiro

--- login/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm 
-
-# Create your views here.
-def login_page(request):
-    return render(request, 'login.html', {
-        "form": UserCreationForm
-    })
