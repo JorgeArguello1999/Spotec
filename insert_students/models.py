7e4c6142c696e63f60f9c48b4e01411f68ea0331
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-11-06 00:38:16 -0500

    feat: Creado modelo para el registro primario de los estudiantes

--- insert_students/models.py
+++ insert_students/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class create_student(models.Model):
+    id = models.AutoField(primary_key=True)
+    cedula = models.CharField(max_length=10)
+    nombre = models.CharField(max_length=255)
+    provincia = models.CharField(max_length=255, null=True)
+    escuela = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return self.nombre
