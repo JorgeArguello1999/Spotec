529a20aeef5f3b05177e8281cb069695b87e1e7b
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-11-05 20:50:43 -0500

    work: Trabajando en el modulo de LogIn

--- swimming/asgi.py
+++ swimming/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for swing project.
+ASGI config for swimming project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "swing.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "swimming.settings")
 
 application = get_asgi_application()
--- swimming/urls.py
+++ swimming/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for swing project.
+URL configuration for swimming project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# Views
+from login import views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("login/", views.login_page, name="login")
 ]
