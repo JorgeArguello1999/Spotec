56b42478d6a2582a415bcbe957bcf2992ab70e44
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-11-08 18:39:08 -0500

    feat: Modificando la base de datos para acoplarse a las exigencias

--- insert_students/forms.py
+++ insert_students/forms.py
@@ -4,8 +4,29 @@ from .models import Estudiante
 class Estudiante_form(ModelForm):
     class Meta:
         model = Estudiante 
-        fields = ["nombre", "cedula", "provincia", 
-            "escuela", "mariposa_hombre", "espalda_hombre",
-            "libre_hombre", "mariposa_mujer", "espalda_mujer",
-            "libre_mujer"
+        fields = [
+                "id",
+                "cedula",
+                "nombre",
+                "provincia",
+                "escuela",
+                "categoria",
+                "genero",
+                "combinado_100m",
+                "espalda_25m",
+                "espalda_50m",
+                "espalda_100m",
+                "mariposa_25m",
+                "mariposa_50m",
+                "mariposa_100m",
+                "libre_25m_tarde",
+                "libre_50m_tarde",
+                "libre_100m_tarde",
+                "pecho_25m_tarde",
+                "pecho_50m_tarde",
+                "pecho_100m_tarde",
+                "relevos_4x50_libre_mixto",
+                "snorkel_libre_25m",
+                "snorkel_libre_50m",
+                "snorkel_libre_100m"
         ]
--- insert_students/models.py
+++ insert_students/models.py
@@ -1,5 +1,29 @@
 from django.db import models
 
+Genero = (
+    ("M", "Masculino"),
+    ("F", "Femenino"),
+)
+
+Categorias = (
+    ("MEN", "Menores 6 años"),
+    ("7-8", "7-8 años"),
+    ("9NO", "9 años novatos"),
+    ("9CL", "9 años clasificados"),
+    ("10N", "10 años novatos"),
+    ("10C", "10 años clasificados"),
+    ("11N", "11 años novatos"),
+    ("11C", "11 años clasificados"),
+    ("12N", "12 años novatos"),
+    ("12C", "12 años clasificados"),
+    ("34N", "13-14 años novatos"),
+    ("34C", "13-14 años clasificados"),
+    ("57N", "15-17 años novatos"),
+    ("57C", "15-17 años clasificados"),
+    ("18N", "18 en adelante novatos"),
+    ("18C", "18 en adelante clasificados")
+)
+
 # Create your models here.
 class Estudiante(models.Model):
     id = models.AutoField(primary_key=True)
@@ -7,17 +31,35 @@ class Estudiante(models.Model):
     nombre = models.CharField(max_length=255)
     provincia = models.CharField(max_length=255, null=True)
     escuela = models.CharField(max_length=255, null=True)
+    genero = models.CharField(max_length=1, choices=Genero)
+    categoria = models.CharField(max_length=3, choices=Categorias)
 
     # Desde aquí se puede modificar dependiendo que pruebas existan
-    mariposa_hombre = models.FloatField(null=True, blank=True)
-    espalda_hombre = models.FloatField(null=True, blank=True)
-    braza_hombre = models.FloatField(null=True, blank=True)
-    libre_hombre = models.FloatField(null=True, blank=True)
+    # Las categorias a las que se inscriban va a depender de su edad
 
-    mariposa_mujer = models.FloatField(null=True, blank=True)
-    espalda_mujer = models.FloatField(null=True, blank=True)
-    braza_mujer = models.FloatField(null=True, blank=True)
-    libre_mujer = models.FloatField(null=True, blank=True)
+    # Eventos en la mañana 
+    combinado_100m = models.FloatField(blank=True)
+    
+    espalda_25m = models.FloatField(blank=True)
+    espalda_50m = models.FloatField(blank=True)
+    espalda_100m = models.FloatField(blank=True)
 
-    def __str__(self):
-        return self.nombre
+    mariposa_25m = models.FloatField(blank=True)
+    mariposa_50m = models.FloatField(blank=True)
+    mariposa_100m = models.FloatField(blank=True)
+
+    # Eventos en la tarde
+    libre_25m_tarde = models.FloatField(blank=True)
+    libre_50m_tarde = models.FloatField(blank=True)
+    libre_100m_tarde = models.FloatField(blank=True)
+
+    pecho_25m_tarde = models.FloatField(blank=True)
+    pecho_50m_tarde = models.FloatField(blank=True)
+    pecho_100m_tarde = models.FloatField(blank=True)
+
+    # Extras
+    relevos_4x50_libre_mixto = models.FloatField(blank=True)
+
+    snorkel_libre_25m = models.FloatField(blank=True)
+    snorkel_libre_50m = models.FloatField(blank=True)
+    snorkel_libre_100m = models.FloatField(blank=True)
