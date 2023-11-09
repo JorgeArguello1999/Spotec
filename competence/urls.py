520fb13827a564378469ecac11c2cf7eb7ab8c35
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-11-09 16:42:10 -0500

    feat: Listando personas que participaran en las competencias

--- competence/urls.py
+++ competence/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.list, name="list"),
+    path("<str:distancia>/<str:genero>/<str:categoria>/<str:prueba>", views.list_filter, name="list_filter"),
     path("create/", views.create, name="create")
 ]
--- competence/views.py
+++ competence/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from .models import Competencia
 from . import functions
 
-# Create your views here.
 def list(request):
     lista = Competencia.objects.all()
-    print(lista)
-    # functions.insert_student_in_compentence(lista)
     return render(request, 'list_competence.html', {
-        "lista": "lista"
+        "lista": lista,
+        "titulo": "Todas las competencias"
+    })
+
+def list_filter(request, distancia, genero, categoria, prueba):
+    lista = Competencia.objects.filter(distancia=distancia, genero=genero, categoria=categoria, prueba=prueba)
+    return render(request, 'list_competence.html', {
+        "lista": lista,
+        "titulo": f"{genero} - {categoria} - {prueba} - {distancia}"
     })
 
 def create(request):
