c191c9c1b62fc2e32de86413dfb7eb4c8fa419a6
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-11-13 04:59:22 -0500

    fix: Solucionado, se puede actualizar la tabla de campeones

--- winners/functions.py
+++ winners/functions.py
@@ -7,7 +7,9 @@ def create_ganadores(data):
         distancia=data["distancia"], genero=data["genero"], categoria=data["categoria"], prueba=data["prueba"]
     ).order_by("tiempo_competencia")
 
-    if "REL" in data["especial"]: competidores = Competencia.objects.filter(categoria=data["categoria"], prueba=data["prueba"]).order_by("tiempo_competencia")
+    if "REL" in data["especial"]: competidores = Competencia.objects.filter(
+        categoria=data["categoria"], prueba=data["prueba"]
+    ).order_by("tiempo_competencia")
 
     if "COM" in data["especial"]: competidores = Competencia.objects.filter(
         genero=data["genero"], categoria=data["categoria"], prueba=data["prueba"]
@@ -16,25 +18,38 @@ def create_ganadores(data):
     puesto = 1
     puntos = 9
     for i in competidores:
-        datos = {
-            "id": i.id,
-            "cedula": i.cedula,
-            "nombre": i.nombre,
-            "provincia": i.provincia,
-            "escuela": i.escuela,
-            "genero": i.genero,
-            "categoria": i.categoria,
-            "prueba": i.prueba,
-            "distancia": i.distancia,
-            "tiempo_registro": i.tiempo_registro,
-            "tiempo_competencia": i.tiempo_competencia,
-            "puesto": puesto,
-            "puntaje": puntos 
-        }
-        if puesto == 1: puntos = puntos - 2
-        elif puesto == 0: puntos = 0
-        else: puntos = puntos - 1 
-        puesto = puesto + 1 
+        if i.tiempo_competencia == None or i.tiempo_competencia == float(0):
+            pass
+        else:
+            datos = {
+                "id": i.id,
+                "cedula": i.cedula,
+                "nombre": i.nombre,
+                "provincia": i.provincia,
+                "escuela": i.escuela,
+                "genero": i.genero,
+                "categoria": i.categoria,
+                "prueba": i.prueba,
+                "distancia": i.distancia,
+                "tiempo_registro": i.tiempo_registro,
+                "tiempo_competencia": i.tiempo_competencia,
+                "puesto": puesto,
+                "puntaje": puntos 
+            }
+            if puesto == 1: puntos = puntos - 2
+            elif puesto == 0: puntos = 0
+            else: puntos = puntos - 1 
+            puesto = puesto + 1 
 
-        # Falla al actualizar el registro
-        Ganadores.objects.update_or_create(**datos)
+            # Falla al actualizar el registro
+            try:
+                Ganadores.objects.update_or_create(**datos)
+            except:
+                obj = Ganadores.objects.get(id=i.id)
+                obj.delete()
+                Ganadores.objects.create(**datos)
+
+    return {
+        "distancia":data["distancia"], "genero":data["genero"], 
+        "categoria":data["categoria"], "prueba":data["prueba"]
+    }
--- winners/views.py
+++ winners/views.py
@@ -15,17 +15,14 @@ def list_all(request):
 
 def list_filter(request, distancia, genero, categoria, prueba):
     lista = Ganadores.objects.filter(distancia=distancia, genero=genero, categoria=categoria, prueba=prueba)
-    especial = "NO"
 
     if prueba == "REL":
         # En relevos se aceptan Hombre y Mujeres y la distancia es fija
         lista = Ganadores.objects.filter(categoria=categoria, prueba=prueba)
-        especial = "REL, no Genero, no distancia"
     
     if prueba == "COM":
         # En combinados la distancia es fija
         lista = Ganadores.objects.filter(genero=genero, categoria=categoria, prueba=prueba)
-        especial = "COM, no distancia"
 
     lista = lista.order_by("puesto")
     return render(request, 'list_winners.html', {
@@ -35,6 +32,8 @@ def list_filter(request, distancia, genero, categoria, prueba):
 
 def create(request):
     if request.method != "GET":
-        create_ganadores(request.POST)
+        salida = create_ganadores(request.POST)
 
-    return redirect("list_winners") 
+    return redirect("list_filter", distancia=salida["distancia"], genero=salida["genero"],
+        categoria=salida["categoria"], prueba=salida["prueba"]
+    ) 
