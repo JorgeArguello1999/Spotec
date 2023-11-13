a833fbf6f1603657e3b018ca9f007cf9e2172291
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-11-13 02:48:41 -0500

    feat: Funciona el sistema de Puntuación

--- winners/functions.py
+++ winners/functions.py
@@ -1,15 +1,55 @@
 from .models import Ganadores
 from competence.models import Competencia
-import json
+
+"""
+
+    genero = models.CharField(max_length=1)
+    categoria = models.CharField(max_length=3)
+    prueba = models.CharField(max_length=3)
+    distancia = models.CharField(max_length=3)
+
+    tiempo_registro = models.FloatField(blank=True, null=True)
+    tiempo_competencia = models.FloatField(blank=True, null=True)
+
+    puesto = models.IntegerField(blank=True, null=True)
+    puntaje = models.IntegerField(blank=True, null=True)
+
+
+"""
 
 def create_ganadores(data):
     competidores = None
     if data["especial"] == "NO": competidores = Competencia.objects.filter(
         distancia=data["distancia"], genero=data["genero"], categoria=data["categoria"], prueba=data["prueba"]
-    )
+    ).order_by("tiempo_competencia")
 
-    if "REL" in data["especial"]: competidores = Competencia.objects.filter(categoria=data["categoria"], prueba=data["prueba"])
+    if "REL" in data["especial"]: competidores = Competencia.objects.filter(categoria=data["categoria"], prueba=data["prueba"]).order_by("tiempo_competencia")
 
-    if "COM" in data["especial"]: competidores = Competencia.objects.filter(genero=data["genero"], categoria=data["categoria"], prueba=data["prueba"])
+    if "COM" in data["especial"]: competidores = Competencia.objects.filter(
+        genero=data["genero"], categoria=data["categoria"], prueba=data["prueba"]
+    ).order_by("tiempo_competencia")
 
-    print(competidores)
+    puesto = 1
+    puntos = 9
+    for i in competidores:
+        datos = {
+            "id_student": i.id,
+            "cedula": i.cedula,
+            "nombre": i.nombre,
+            "provincia": i.provincia,
+            "escuela": i.escuela,
+            "genero": i.genero,
+            "categoria": i.categoria,
+            "prueba": i.prueba,
+            "distancia": i.distancia,
+            "tiempo_registro": i.tiempo_registro,
+            "tiempo_competencia": i.tiempo_competencia,
+            "puesto": puesto,
+            "puntaje": puntos 
+        }
+        if puesto == 1: puntos = puntos - 2
+        else: puntos = puntos - 1 
+        puesto = puesto + 1 
+
+        objeto = Ganadores.objects.create(**datos)
+        objeto.save()
