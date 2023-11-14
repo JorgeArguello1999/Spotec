65f516ad4e37f4d37cb265f139c624bc5134476c
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-11-14 08:49:35 -0500

    USABLE al 100% Tener en cuenta el campo de escuela inferior

--- insert_students/views.py
+++ insert_students/views.py
@@ -5,6 +5,7 @@ from .models import Estudiante
 from .forms import Estudiante_form
 from competence import functions
 from competence.models import Competencia
+from schools.models import Escuelas
 
 # Create your views here.
 @login_required
@@ -20,13 +21,52 @@ def create(request):
         # Pasamos los datos obtenidos a una función para insertarlo en la tabla competencia
         functions.insert_student_in_compentence(request.POST)
 
+        data_dict = {
+            'cedula': request.POST['cedula'],
+            'nombre': request.POST['nombre'],
+            'provincia': request.POST.get('provincia', None),
+            'escuela': request.POST.get('escuela', None),
+            'genero': request.POST['genero'],
+            'categoria': request.POST['categoria'],
+            'manana1_prueba': request.POST.get('manana1_prueba', None),
+            'manana1_distancia': request.POST.get('manana1_distancia', None),
+            'manana1_tiempo': request.POST.get('manana1_tiempo', None),
+            'manana2_prueba': request.POST.get('manana2_prueba', None),
+            'manana2_distancia': request.POST.get('manana2_distancia', None),
+            'manana2_tiempo': request.POST.get('manana2_tiempo', None),
+            'tarde1_prueba': request.POST.get('tarde1_prueba', None),
+            'tarde1_distancia': request.POST.get('tarde1_distancia', None),
+            'tarde1_tiempo': request.POST.get('tarde1_tiempo', None),
+            'tarde2_prueba': request.POST.get('tarde2_prueba', None),
+            'tarde2_distancia': request.POST.get('tarde2_distancia', None),
+            'tarde2_tiempo': request.POST.get('tarde2_tiempo', None),
+            'opcional1_prueba': request.POST.get('opcional1_prueba', None),
+            'opcional1_distancia': request.POST.get('opcional1_distancia', None),
+            'opcional1_tiempo': request.POST.get('opcional1_tiempo', None),
+            'opcional2_prueba': request.POST.get('opcional2_prueba', None),
+            'opcional2_distancia': request.POST.get('opcional2_distancia', None),
+            'opcional2_tiempo': request.POST.get('opcional2_tiempo', None),
+        }
+        print(request.POST)
+        print(data_dict)
+
         # Guardamos al Estudiante en la tabla estudiante
-        salida = Estudiante_form(request.POST)
-        salida.save()
+        try:
+            salida = Estudiante_form(data_dict)
+            salida.save()
+        except:
+            salida = Estudiante.objects.create(data_dict)
+            salida.save()
         return redirect("list_insert_students")
     
+    # Devolvemos el formulario
+    escuelas_choices = [('','---------')] + [(escuela.nombre, escuela.nombre) for escuela in Escuelas.objects.all()]
+    form = Estudiante_form()
+    form.fields['escuela'].choices = escuelas_choices
+    
     return render(request, 'create_insert_students.html', {
-        "form": Estudiante_form
+        "form": Estudiante_form,
+        "escuelas_choices": escuelas_choices
     })
 
 @login_required
