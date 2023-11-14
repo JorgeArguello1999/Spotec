780c6d101e589c88c07a29cc7771193dd20962f5
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2023-11-13 19:53:37 -0500

    feat: Arreglando la selección de Escuelas existentes

--- insert_students/forms.py
+++ insert_students/forms.py
@@ -1,11 +1,17 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Estudiante
+from schools.models import Escuelas
+from competence.functions import Genero, Categorias, PruebasManana, PruebasTarde, PruebasOpcionales, Distancia
 
-class Estudiante_form(ModelForm):
+class Estudiante_form(forms.ModelForm):
+    escuelas_choices = [('','---------')] + [(escuela.nombre, escuela.nombre) for escuela in Escuelas.objects.all()]
+    escuela = forms.ChoiceField(choices=escuelas_choices, required=False)
+
+    
     class Meta:
         model = Estudiante 
         fields = [
-            "id", "cedula", "nombre", "provincia", "escuela", "categoria", "genero",
+            "nombre", "cedula", "provincia", "escuela", "categoria", "genero",
             "manana1_prueba", "manana1_distancia", "manana1_tiempo",
             "manana2_prueba", "manana2_distancia", "manana2_tiempo",
             "tarde1_prueba", "tarde1_distancia", "tarde1_tiempo",
@@ -13,3 +19,24 @@ class Estudiante_form(ModelForm):
             "opcional1_prueba", "opcional1_distancia", "opcional1_tiempo",
             "opcional2_prueba", "opcional2_distancia", "opcional2_tiempo",
         ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        # Añade las opciones predefinidas a los campos de selección
+        for field_name, choices in [
+            ('genero', Genero),
+            ('categoria', Categorias),
+            ('manana1_prueba', PruebasManana),
+            ('manana1_distancia', Distancia),
+            # ... Añade otras opciones predefinidas según sea necesario
+        ]:
+            self.fields[field_name].choices = choices
+
+        # Establece valores predeterminados para los campos de tiempo
+        for field_name in [
+            'manana1_tiempo', 'manana2_tiempo',
+            'tarde1_tiempo', 'tarde2_tiempo',
+            'opcional1_tiempo', 'opcional2_tiempo',
+        ]:
+            self.fields[field_name].initial = 0
